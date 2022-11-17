793daedf659249b64beb685b44b2b91c783434cb
Ovenoboyo <ovenoboyo@gmail.com>
2022-11-17 14:33:21 +0530

    Set moveset properly

--- src/database/pokemon.cpp
+++ src/database/pokemon.cpp
@@ -5,7 +5,6 @@ Pokemon FetchPokemon() {
     Stats stats = {1, 1, 1, 1 ,1 ,1};
     ElementType types[2];
     types[0] = FLYING;
-    //Move moveset[4] = {Move("move1", GROUND, 5, 50, 100)};
     MoveSet moveset = {Move("move1", GROUND, 5, 50, 100)};
 
     return Pokemon("pokeman_test", stats, types, moveset);
